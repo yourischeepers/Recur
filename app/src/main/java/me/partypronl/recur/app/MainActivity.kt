5e0550bedcd3aeb0bf06c2be204c2b018703856b
Youri Scheepers <youri.scheepers@framna.com>
2025-03-26 22:09:03 +0100

    Setup theming

--- app/src/main/java/me/partypronl/recur/app/MainActivity.kt
+++ app/src/main/java/me/partypronl/recur/app/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import me.partypronl.recur.app.theme.RecurTheme
+import me.partypronl.recur.app.theme.AppTheme
 import org.koin.androidx.compose.KoinAndroidContext
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RecurTheme {
+            AppTheme {
                 KoinAndroidContext {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                         Greeting(
@@ -45,7 +45,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    RecurTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
